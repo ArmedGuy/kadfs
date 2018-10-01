7c28eb4e3c850ad388585760a0025e1403aa6578
phnk <carl.borngrund@gmail.com>
2018-10-01 10:33:08 +0200

    fixed broken run file

--- docker-run.sh
+++ docker-run.sh
@@ -5,5 +5,5 @@ docker-compose -f bootstrap-compose.yml up -d
 
 for i in `seq 1 5`;
 do
-    docker-compose -f kademlia-compose.yml up -d
+    docker run --network=testing_kademlia_network -itd kademlia 4000 0000000000000000000000000000000000000000 172.16.238.10:4000
 done
